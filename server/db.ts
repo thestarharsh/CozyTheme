d8f555e4898248ae324df307c530cedcc38b1b4d
thestarharsh <talatiharsh19@gmail.com>
2025-06-13 23:44:25 +0530

    Working Project.

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
